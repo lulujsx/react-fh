0dcb0e6cfbd97b9842a6d1830b25f518fbd35fa6
Luana Vallejos <pukeraimbows@gmail.com>
2024-08-12 22:00:00 -0300

    hello world component

--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -1,10 +1,11 @@
+//estos elementos son los que necesito para renderizar mi app
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { HelloWorldApp } from './HelloWorldApp'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <HelloWorldApp/>
+    </React.StrictMode>
 )
