939e360506f0bf069d4ff956f0028459e49c053a
Luana Vallejos <luanalorenavallejos@gmail.com>
2024-08-18 02:10:20 -0300

    finished 07 test

--- 03-counter-app/src/base-pruebas/08-imp-exp.js
+++ 03-counter-app/src/base-pruebas/08-imp-exp.js
@@ -1,11 +1,5 @@
-// import { heroes } from './data/heroes';
-// import { heroes } from './data/heroes';
-// import heroes, { owners } from '../data/heroes';
+
 import heroes from '../data/heroes';
-
-// console.log( owners );
-
-
 
 
 export const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
